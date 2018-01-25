9148010126f135ce23d9615749684bf15e9b7cba
Aaron Elkins <threcius@yahoo.com>
2018-01-25 14:19:33 +0800

    resizable canvas

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 function init() {
   console.log("Main loaded");
+
   var canvas = document.getElementById('tabs-canvas');
-  var t = new Tab(canvas);
+  window.addEventListener('resize', resizeCanvas, false);
+
+  function resizeCanvas() {
+    render();
+  }
+
+  function render() {
+    var ctx = canvas.getContext('2d');
+    ctx.canvas.width  = window.innerWidth;
+    ctx.canvas.style.width = window.innerWidth + "px";
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'red';
+    ctx.moveTo(0, 28);
+    ctx.lineTo(ctx.canvas.width, 28);
+    ctx.stroke();
+  }
+  //t = new Tab(canvas);
+  //t.draw(true);
+  render();
 }
 
 var tabNumber = 0;
@@ -9,6 +28,8 @@ const tabHeight = 28;
 const tabWidth = 80;
 const defaultTabBackground = "#cecccf";
 const activeTabBackground = "white";
+const deltaXForTabs = 5;
+const radius = 4;
 
 var Tab = function(canvas) {
   this.canvas = canvas;
@@ -16,7 +37,7 @@ var Tab = function(canvas) {
   this.tabNumber = tabNumber + 1;
 
   this.tabRect = {};
-  this.tabRect.x = (tabNumber - 1) * tabWidth;
+  this.tabRect.x = (this.tabNumber - 1) * tabWidth;
   this.tabRect.y = tabHeight;
   this.tabRect.width = tabWidth;
   this.tabRect.height = tabHeight;
@@ -24,8 +45,42 @@ var Tab = function(canvas) {
   this.draw = function(active) {
     this.ctx.save();
 
-    if (active) {
+    const midX = this.tabRect.x + (this.tabRect.width / 2);
+    const midY = this.tabRect.y - (this.tabRect.height / 2);
+    const minX = this.tabRect.x;
+    const minY = this.tabRect.y - this.tabRect.height;
+    const maxX = this.tabRect.x + this.tabRect.width;
+    const maxY = this.tabRect.y;
 
+    const bottomLeft = {x: this.tabRect.x, y: this.tabRect.y};
+    const topLeft = {
+      x: this.tabRect.x,
+      y: this.tabRect.y - this.tabRect.height
+    };
+    const bottomRight = {
+      x: this.tabRect.x + this.tabRect.width,
+      y: this.tabRect.y
+    };
+    const topRight = {
+      x: this.tabRect.x + this.tabRect.width,
+      y: this.tabRect.y - this.tabRect.height
+    };
+
+    // We use arcTo(x1, y1, x2, y2, radius) to construct rounded corners
+    // See http://www.dbp-consulting.com/tutorials/canvas/CanvasArcTo.html
+    if (active) {
+      this.ctx.lineWidth = 1;
+      this.ctx.strokeStyle = 'rgb(50,50,0)';
+      this.ctx.globalAlpha = 1.0;
+    //  this.ctx.beginPath();
+      this.ctx.moveTo(0, 28);
+      this.ctx.lineTo(40, 28);
+      // bottom arc
+      // this.ctx.moveTo(bottomLeft.x, bottomLeft.y);
+      // this.ctx.lineTo(bottomLeft.x + deltaXForTabs + radius, maxY);
+      //this.ctx.arcTo(bottomLeft.x + deltaXForTabs + radius, maxY,
+      //   bottomLeft.x + deltaXForTabs + radius, midY, radius);
+      this.ctx.stroke();
     } else {
 
     }
